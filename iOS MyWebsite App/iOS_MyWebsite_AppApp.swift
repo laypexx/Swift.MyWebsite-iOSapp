d4c12d88b484df1809aa3cd148a071803ef001f2
laypexx <102018524+laypexx@users.noreply.github.com>
2022-09-27 12:13:17 +0200

    Initial Commit

---
+++ iOS MyWebsite App/iOS_MyWebsite_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_MyWebsite_AppApp.swift
+//  iOS MyWebsite App
+//
+//  Created by Dominic Sprenger on 27.09.22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_MyWebsite_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
